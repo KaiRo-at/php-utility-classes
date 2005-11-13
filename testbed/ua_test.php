e742e50704b7b09c6693b8d8284bf8a558b411a9
robert <robert>
2005-11-13 20:44:08 +0000

    update testbed

--- testbed/ua_test.php
+++ testbed/ua_test.php
@@ -16,23 +16,27 @@ else {
 print("<h1>KaiRo's Browser-Test</h1>\n");
 
 print("I read the following user agent string from ".(strlen($httpvars["ua"])?"your input":"your browser").":\n<br>");
-print("<b>".$ua->uastring."</b>\n");
+print("<b>".$ua->getUAString()."</b>\n");
 
-print("<br><br>This is <b>".($ua->isns()?"a":"no")."</b> Netscape browser.\n");
-print("<br>This is <b>".($ua->isns4()?"a":"no")."</b> Netscape Communicator 4 browser.\n");
-print("<br>This is <b>".($ua->isie()?"an":"no")."</b> Internet Explorer browser.\n");
-print("<br>This is <b>".($ua->geckobased()?"a":"no")."</b> Gecko-based browser.\n");
-print("<br>This is <b>".($ua->khtmlbased()?"a":"no")."</b> KHTML-based browser.\n");
-print("<br>This is <b>".($ua->isbot()?"an":"no")."</b> automated robot.\n");
-print("<br>The browser brand is reported as &quot;<b>".$ua->getBrand()."</b>&quot;\n");
+print("<br><br>The browser brand is reported as &quot;<b>".$ua->getBrand()."</b>&quot;\n");
 print("<br>The browser version is reported as &quot;<b>".$ua->getVersion()."</b>&quot;\n");
-if ($ua->geckobased()) { print("<br>The Gecko date is reported as &quot;<b>".$ua->geckodate()."</b>&quot;\n"); }
+print("<br>The browser engine is reported as &quot;<b>".$ua->getEngine()."</b>&quot;\n");
+print("<br>The engine version is reported as &quot;<b>".$ua->getEngineVersion()."</b>&quot;\n");
+print("<br>The operating system is reported as &quot;<b>".$ua->getOS()."</b>&quot;\n");
+print("<br>The system platform is reported as &quot;<b>".$ua->getPlatform()."</b>&quot;\n");
+print("<br>The browser language is reported as &quot;<b>".$ua->getLanguage()."</b>&quot;\n");
+if ($ua->geckobased()) { print("<br>The Gecko date is reported as &quot;<b>".$ua->getGeckoDate()."</b>&quot;\n"); }
 print("<br><br>I conclude this must be <b>".$ua->getBrand()." ".$ua->getVersion()."</b>\n");
+print("<br>This is <b>".($ua->isBot()?"an":"no")."</b> automated robot.\n");
+
+$acclang = $ua->getAcceptLanguages();
+print("<br><br>Accepted Languages: ");
+foreach ($acclang as $lang=>$q) { print($lang."(".$q.") "); }
+print("\n");
 
 print("<br><br>Test the following UA string (leave empty to read it from your browser):\n");
 print("<form method=\"POST\" action=\"\"><p>\n");
-print("<input type=\"text\" name=\"ua\" value=\"".$ua->uastring."\" size=\"80\" maxlength=\"150\">\n");
+print("<input type=\"text\" name=\"ua\" value=\"".htmlentities($ua->uastring)."\" size=\"80\" maxlength=\"150\">\n");
 print("<br><input type=\"submit\" value=\"Test\"></p></form>\n");
-
 $wrapper->pgbottom();
 ?>

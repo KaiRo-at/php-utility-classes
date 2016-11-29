880bcb60d557851e01051e9382decd36ec7665a1
Robert Kaiser <kairo@kairo.at>
2016-11-29 18:24:20 +0100

    put clear MPL2 license headers on all files

--- tests/ua_test.php
+++ tests/ua_test.php
@@ -1,4 +1,8 @@
 <?php
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 include('../classes/useragent.php-class');
 
 // set default time zone - right now, always the one the server is in!
